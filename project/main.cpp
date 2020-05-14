3a206cd7804c2012067cc3b535ac6df7e7e22c34
MorozovGZ <210.06@mail.ru>
2020-05-14 21:03:59 +0300

    Обработка ошибок

--- project/main.cpp
+++ project/main.cpp
@@ -166,15 +166,14 @@ int main(int argc, char* argv[] )
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if ( res != CURLE_OK )
+            {
+                cerr << curl_easy_strerror( res ) << endl;
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
 
-        cout << " argc = " << argc << endl;
-        for (int i = 0; i < argc; ++i)
-        {
-
-            cout << "argv[" << i << "] = " << argv[i] << " ";
-        }
     }
     return 0;
 }
