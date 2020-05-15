b0d20eae87797c1f65fb593af9ab7d1aea4beb70
MorozovGZ <210.06@mail.ru>
2020-05-15 17:14:37 +0300

    Обновлённая версия Д/З

--- project/svg.cpp
+++ project/svg.cpp
@@ -36,7 +36,7 @@ void svg_text(double left, double baseline, size_t text)
     cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
 }
 
-void svg_rect(double x, double y, double width, double height, string stroke, string filll)
+void svg_rect(double x, double y, double width, double height, string stroke, string fill)
 {
     cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
 }
@@ -45,15 +45,17 @@ void svg_rect(double x, double y, double width, double height, string stroke, st
 
 void show_histogram_svg(const vector<size_t>& bins)
 {
-double TEXT_WIDTH = 0;
+    const auto TEXT_HEIGHT = 75;
+    double TEXT_WIDTH = 10;
     const auto IMAGE_WIDTH = 400;
     const auto IMAGE_HEIGHT = 500;
     const auto TEXT_LEFT = 10;
-    const auto TEXT_BASELINE = 120;
-const auto CONST_AUTO = 20;
+    const auto TEXT_BASELINE = 450;
+    const auto CONST_AUTO = 20;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
     const size_t MAX_ASTERISK = IMAGE_WIDTH - TEXT_LEFT - TEXT_WIDTH;
+    const auto BAR_HEIGHT = IMAGE_HEIGHT - TEXT_HEIGHT;
 
     size_t max_count = 0;
     for (size_t count : bins)
@@ -69,22 +71,25 @@ const auto CONST_AUTO = 20;
 
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-double HEIGHT_MAXIMUM = find_max(bins);
-double top = 0;
+    double HEIGHT_MAXIMUM = find_max(bins);
+    double top = 0;
 
-        for (size_t bin : bins)
+     double scaling_factor = (double)MAX_ASTERISK / (max_count * BLOCK_WIDTH);
+
+    for (size_t bin : bins)
     {
 
-        size_t height = bin;
-        if (scaling_needed)
-        {
-            const double scaling_factor = (double)MAX_ASTERISK / (max_count * BLOCK_WIDTH);
-            height = (size_t)(bin * scaling_factor);
-        }
+
+        scaling_factor = (double)bin / (double) max_count;
+        size_t height = scaling_factor * BAR_HEIGHT;
+       double y = BAR_HEIGHT - height;
+
+
 
         const double bin_width = BLOCK_WIDTH * height;
         svg_text(top + TEXT_LEFT,TEXT_BASELINE, bin);
-        svg_rect(TEXT_WIDTH, CONST_AUTO*(HEIGHT_MAXIMUM - height), BIN_HEIGHT, height*CONST_AUTO,"red","#aab5ff");
+
+        svg_rect(TEXT_WIDTH, y, BIN_HEIGHT, height,"red","#aab5ff");
         TEXT_WIDTH += BIN_HEIGHT;
         top += BIN_HEIGHT;
 
