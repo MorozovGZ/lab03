412f085b9ea63fe038f6fd334a9ea5c22973531f
MorozovGZ <210.06@mail.ru>
2020-06-01 00:10:38 +0300

    Revert "printf"
    This reverts commit 19051388a6217befe0158fe823c8b1efdf74a1df.

--- project/main.cpp
+++ project/main.cpp
@@ -6,11 +6,6 @@
 
 using namespace std;
 
-
-
-
-
-
 vector<double> input_numbers(const size_t count)
 {
     vector<double> result(count);
@@ -103,12 +98,12 @@ svg_end()
 }
 void svg_text(double left,double baseline, string text)
 {
-    cout<< "<text x='" << left << "' y='" << baseline << "'>'"<< text << "'</text>";
+cout<< "<text x='" << left << "' y='" << baseline << "'>'"<< text << "'</text>";
 }
 
 void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
 {
-    cout<< "<rect x='" << x << "' y='"<< y << "' width='" << width << "' height='" << height << "' stroke='red' fill='blue'/>";
+cout<< "<rect x='" << x << "' y='"<< y << "' width='" << width << "' height='" << height << "' stroke='red' fill='blue'/>";
 }
 void
 show_histogram_svg(const vector<size_t>& bins)
@@ -116,25 +111,24 @@ show_histogram_svg(const vector<size_t>& bins)
     svg_begin(400, 300);
     svg_text(20,20,to_string(bins[0]));
     const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    double top = 0;
-    for (size_t bin : bins)
-    {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-        top += BIN_HEIGHT;
-    }
+const auto IMAGE_HEIGHT = 300;
+const auto TEXT_LEFT = 20;
+const auto TEXT_BASELINE = 20;
+const auto TEXT_WIDTH = 50;
+const auto BIN_HEIGHT = 30;
+const auto BLOCK_WIDTH = 10;
+double top = 0;
+for (size_t bin : bins) {
+const double bin_width = BLOCK_WIDTH * bin;
+svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+top += BIN_HEIGHT;
+}
     svg_end();
 }
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height,
-              string stroke, string fill);
+        string stroke, string fill);
 
 
 
@@ -157,11 +151,6 @@ int main()
     const auto bins = make_histogram(numbers, bin_count);
 
     show_histogram_svg(bins);
-     const char* name = "Commander Shepard";
-    int year = 2154;
-    printf("%s was born in %d.\n", name, year);
-    printf("n = %08x\n", 0x1234567);
-    return 0;
 
     return 0;
 }
