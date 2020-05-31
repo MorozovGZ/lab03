d7e3ad82d9cf6195338059becf1936f1eeb7bcf8
MorozovGZ <210.06@mail.ru>
2020-06-01 01:35:30 +0300

    Получение имени компьютера

--- project/main.cpp
+++ project/main.cpp
@@ -8,6 +8,37 @@
 
 using namespace std;
 
+string print_name ()
+{
+    stringstream buffer;
+
+    DWORD info = GetVersion();
+    DWORD mask = 0x0000ffff;
+    DWORD version = info & mask;
+
+    DWORD platform = info >> 16;
+    DWORD mask_minor = 0x000000ff;
+    DWORD mask_major = 0x0000ff00;
+    DWORD version_minor = info & mask_minor;
+
+
+    DWORD version_major1 = info & mask_major;
+    DWORD version_major = version_major1>>8;
+
+
+    if ((info & 0b10000000'00000000'0000000'00000000) == 0)
+    {
+        DWORD build = platform;
+        buffer << "Windows v" << version_minor << "." << version_major << " (build" << build << ")\n";
+
+    }
+    char system [MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD Size =sizeof (system);
+    GetComputerNameA(system, &Size);
+    buffer << "Name: "<<system<<"\n";
+
+    return buffer.str();
+}
 vector<double> input_numbers(const size_t count)
 {
     vector<double> result(count);
@@ -21,7 +52,7 @@ vector<double> input_numbers(const size_t count)
 
 
 
- vector<size_t> make_histogram(const vector<double>& numbers, const size_t count)
+vector<size_t> make_histogram(const vector<double>& numbers, const size_t count)
 {
     vector<size_t> result(count);
     double min;
@@ -100,37 +131,39 @@ svg_end()
 }
 void svg_text(double left,double baseline, string text)
 {
-cout<< "<text x='" << left << "' y='" << baseline << "'>'"<< text << "'</text>";
+    cout<< "<text x='" << left << "' y='" << baseline << "'>'"<< text << "'</text>";
 }
 
 void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
 {
-cout<< "<rect x='" << x << "' y='"<< y << "' width='" << width << "' height='" << height << "' stroke='red' fill='blue'/>";
+    cout<< "<rect x='" << x << "' y='"<< y << "' width='" << width << "' height='" << height << "' stroke='red' fill='blue'/>";
 }
 void
 show_histogram_svg(const vector<size_t>& bins)
 {
     svg_begin(400, 300);
     svg_text(20,20,to_string(bins[0]));
-    const auto IMAGE_WIDTH = 400;
-const auto IMAGE_HEIGHT = 300;
-const auto TEXT_LEFT = 20;
-const auto TEXT_BASELINE = 20;
-const auto TEXT_WIDTH = 50;
-const auto BIN_HEIGHT = 30;
-const auto BLOCK_WIDTH = 10;
-double top = 0;
-for (size_t bin : bins) {
-const double bin_width = BLOCK_WIDTH * bin;
-svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-top += BIN_HEIGHT;
-}
+    const auto IMAGE_WIDTH = 1000;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    double top = 0;
+    for (size_t bin : bins)
+    {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
+    }
+     cout << "<text x='" << TEXT_LEFT << "' y='" << BIN_HEIGHT+top << "'>" << print_name() <<"</text>";
     svg_end();
 }
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height,
-        string stroke, string fill);
+              string stroke, string fill);
 
 
 
@@ -138,28 +171,8 @@ void svg_rect(double x, double y, double width, double height,
 
 int main()
 {
-     printf("Decimal version %u\n", GetVersion());
-    printf("Hexadecimal version %x\n", GetVersion());
-    DWORD info = GetVersion();
-    DWORD mask = 0x0000ffff;
-    DWORD version = info & mask;
-    printf("version %u.\n", version);
 
-    DWORD platform = info >> 16;
-    DWORD mask_minor = 0x000000ff;
-    DWORD mask_major = 0x0000ff00;
-    DWORD version_minor = info & mask_minor;
-    printf("version minor %u.\n", version_minor);
-
-    DWORD version_major1 = info & mask_major;
-    DWORD version_major = version_major1>>8;
-    printf("version major %u.\n",version_major);
-
-    if ((info & 0b10000000'00000000'0000000'00000000) == 0) {
-    DWORD build = platform;
-    printf("build %u.\n",build);
-}
-    /* size_t number_count;
+     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
@@ -174,6 +187,6 @@ int main()
     const auto bins = make_histogram(numbers, bin_count);
 
     show_histogram_svg(bins);
-*/
+
     return 0;
 }
